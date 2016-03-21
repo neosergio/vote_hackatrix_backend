2a8da1601242450f356f966a874e9f6f04851a4f
Sergio Infante <rsinfante@gmail.com>
2016-03-21 15:49:59 -0500

    Add ideas application to project.

---
+++ ideas/models.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Idea(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.name
