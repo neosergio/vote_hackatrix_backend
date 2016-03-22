7405bda939632a5f8cac93413b4e99939ef716c2
Sergio Infante <raulsergio9@gmail.com>
2016-03-21 22:30:39 -0500

    Add unique parameter to idea name

--- ideas/models.py
+++ ideas/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Idea(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     description = models.TextField()
     votes = models.IntegerField(default=0)
 
