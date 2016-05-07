fa8d62a009852936a9391f6dbd102dc6e69fbbad
Sergio Infante <raulsergio9@gmail.com>
2016-05-07 08:12:07 -0500

    Update models and sample data

--- ideas/admin.py
+++ ideas/admin.py
@@ -1,9 +1,24 @@
-from .models import Idea
+from .models import Idea, Outstanding
 from django.contrib import admin
 
 
 class IdeaAdmin(admin.ModelAdmin):
-    list_display = ('name', 'votes', 'description')
+    list_display = ('name', 'votes', 'description', 'register', 'is_active')
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'register', None) is None:
+            obj.register = request.user
+        obj.save()
+
+
+class OutstandingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'comment', 'register')
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'register', None) is None:
+            obj.register = request.user
+        obj.save()
 
 
 admin.site.register(Idea, IdeaAdmin)
+admin.site.register(Outstanding, OutstandingAdmin)
