8f43560f69342369013f4ffc0c63c68552871c1a
Sergio Infante <raulsergio9@gmail.com>
2016-05-07 09:22:18 -0500

    Change order from oustanding member to idea

--- ideas/models.py
+++ ideas/models.py
@@ -4,23 +4,23 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Outstanding(models.Model):
-    name = models.CharField(max_length=200, unique=True)
-    email = models.EmailField()
-    comment = models.TextField(null=True, blank=True)
-    register = models.ForeignKey(User, null=True, blank=True, editable=False)
-
-    def __unicode__(self):
-        return self.name
-
-
 class Idea(models.Model):
     name = models.CharField(max_length=200, unique=True)
     description = models.TextField()
     votes = models.IntegerField(default=0)
     register = models.ForeignKey(User, null=True, blank=True, editable=False)
-    outstanding_member = models.ForeignKey(Outstanding, null=True, blank=True)
     is_active = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Outstanding(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    email = models.EmailField()
+    comment = models.TextField()
+    idea = models.ForeignKey(Idea)
+    register = models.ForeignKey(User, null=True, blank=True, editable=False)
 
     def __unicode__(self):
         return self.name
