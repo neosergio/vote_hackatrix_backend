8ccaed4590cb89d95b949d71934ee198f1b0574e
Sergio Infante <rsinfante@gmail.com>
2016-05-16 14:45:01 -0500

    Add UserAdmin and GroupAdmin to custom AdminSite

--- ideas/admin.py
+++ ideas/admin.py
@@ -1,6 +1,7 @@
 from .models import Idea, Outstanding
 from django.contrib import admin
 from django.contrib.admin import AdminSite
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
 from django.contrib.auth.models import User, Group
 
 
@@ -29,7 +30,7 @@ class OutstandingAdmin(admin.ModelAdmin):
 
 
 admin_site = MyAdminSite(name='myadmin')
-admin_site.register(User)
-admin_site.register(Group)
+admin_site.register(User, UserAdmin)
+admin_site.register(Group, GroupAdmin)
 admin_site.register(Idea, IdeaAdmin)
 admin_site.register(Outstanding, OutstandingAdmin)
